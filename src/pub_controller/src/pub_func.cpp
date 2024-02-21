ab16b444aa1e1a17ead3729afa404de20a93665d
Etern1tyDark <nathankhopancras@gmail.com>
2024-02-21 19:16:48 +0700

    change msg location

--- src/pub_controller/src/pub_func.cpp
+++ src/pub_controller/src/pub_func.cpp
@@ -20,7 +20,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
-#include "pub_controller/msg/motion.hpp"
+#include "obj_msg/msg/motion.hpp"
 
 using std::placeholders::_1;
 
@@ -63,7 +63,7 @@ public:
   Controller()
   : Node("controller")
   {
-    publisher_ = this->create_publisher<pub_controller::msg::Motion>("commands", 10);
+    publisher_ = this->create_publisher<obj_msg::msg::Motion>("commands", 10);
     subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>(
         "joy", 10, std::bind(&Controller::topic_callback, this, _1)
     );
@@ -72,7 +72,7 @@ public:
 private:
   void topic_callback(const sensor_msgs::msg::Joy &msg)
   {
-    auto motion_data = pub_controller::msg::Motion();
+    auto motion_data = obj_msg::msg::Motion();
     motion_data.x_cmd = msg.axes[LR_LEFT] * max_value;
     motion_data.y_cmd = msg.axes[LR_RIGHT] * max_value;
 
@@ -82,8 +82,8 @@ private:
     publisher_->publish(motion_data);
     RCLCPP_INFO(this->get_logger(), "Motion commands inputted");
   }
-  rclcpp::Publisher<pub_controller::msg::Motion>::SharedPtr publisher_;
-	rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriber_;
+  rclcpp::Publisher<obj_msg::msg::Motion>::SharedPtr publisher_;
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriber_;
 };
 
 int main(int argc, char * argv[])
